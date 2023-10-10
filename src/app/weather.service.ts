a4a84fbfb7be06888e2fab60093b58773cc64eb9
Simon Rabuogi <symokevo@gmail.com>
2023-10-10 03:18:00 +0300

    add pwa to the app

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -8,7 +8,7 @@ import { Weather } from './weather';
 })
 export class WeatherService {
   private apiUrl = 'https://api.openweathermap.org/data/2.5/';
-  private apiKey = '176443448b738a92cb37295efe75bfdf';
+  private apiKey = 'ed1850018d95fd1db736a3de24e3ae0f';
 
   constructor(private http: HttpClient) {  }
 
