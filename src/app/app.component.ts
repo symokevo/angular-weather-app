fd15843fad1ee9246a871cbf181eadb13fce4773
Simon Rabuogi <symokevo@gmail.com>
2023-10-13 07:19:00 +0300

    Make an update function for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-weather-app';
+
+  constructor(private updates: SwUpdate, private snackbar: MatSnackBar) {}
+  ngOnInit() {
+    this.updates.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      switchMap(() => this.snackbar.open('A new version is available', 'update now').afterDismissed()),
+      filter(result => result.dismissedByAction),
+      map(() => this.updates.activateUpdate().then(() => location.reload()))
+    ).subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
